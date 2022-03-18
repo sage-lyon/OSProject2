c4945728cf892c6fbc9144e8a82e5996c480ad5e
MatthewSylvester1 <Matthew_Sylvester@student.uml.edu>
2022-03-17 22:33:13 -0400

    fixed some erros

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include "prod_cons_MT.c"
 
-m monitor;
+monitor m;
 
+void monitor_init(monitor *m, int * passed_buffer) {
+	m->consumer_location=0;
+	m->producer_location=0;
+	m->buffer_data_stored=0;
+	m->buffer = passed_buffer;
+	
+}
 int main(int argc, char **argv) {
 	
     assert(argc == 4);
@@ -13,7 +21,6 @@ int main(int argc, char **argv) {
     int num_producers = atoi(argv[2]);
     int num_consumers = atoi(argv[3]);
     assert(buffer_size > 0);
-
     int buffer[buffer_size];
 
     pthread_t prod[num_producers];
@@ -21,10 +28,10 @@ int main(int argc, char **argv) {
 
     // Create producer and consumer threads
     for(int i = 0; i < num_producers; i++)
-        pthread_create(&prod[i], NULL, producer(), NULL);
+        pthread_create(&prod[i], NULL, &producer, &i);
     
     for(int i = 0; i < num_consumers; i++)
-        pthread_create(&cons[i], NULL, consumer(), NULL);
+        pthread_create(&cons[i], NULL, &consumer, &i);
 
     // Join producer and consumer threads
     for(int i = 0; i < num_producers; i++)
