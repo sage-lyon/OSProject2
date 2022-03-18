c6e29cadf848dbb71f6082f350b3f819cdbdd61e
sage-lyon <sage_lyon@student.uml.edu>
2022-03-17 20:11:26 -0400

    Added functions to create producer and consumer threads

---
+++ main.c
@@ -0,0 +1,38 @@
+#include "prod_cons_MT.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+int main(int argc, char **argv) {
+
+    assert(argc == 4);
+
+    int buffer_size = atoi(argv[1]);
+    int num_producers = atoi(argv[2]);
+    int num_consumers = atoi(argv[3]);
+
+    assert(buffer_size > 0);
+
+    int buffer[buffer_size];
+
+    pthread_t prod[num_producers];
+    pthread_t cons[num_consumers];
+
+    // Create producer and consumer threads
+    for(int i = 0; i < num_producers; i++)
+        pthread_create(&prod[i], NULL, producer(), NULL);
+    
+    for(int i = 0; i < num_consumers; i++)
+        pthread_create(&cons[i], NULL, consumer(), NULL);
+
+    // Join producer and consumer threads
+    for(int i = 0; i < num_producers; i++)
+        pthread_join(prod[i], NULL);
+    
+    for(int i = 0; i < num_consumers; i++)
+        pthread_join(cons[i], NULL);
+    
+
+    return 0;
+}
