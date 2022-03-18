9e2a8d20f9ae8298a3ede978f7aaa0fdfa303d18
MatthewSylvester1 <Matthew_Sylvester@student.uml.edu>
2022-03-17 22:33:47 -0400

    fixed some errors

--- prod_cons_MT.h
+++ prod_cons_MT.h
@@ -1,10 +1,12 @@
+
 typedef struct __monitor {
     pthread_cond_init(CVP);
 	pthread_cond_init(CVC);
 	pthread_mutex_init(producer_lock);
 	pthread_mutex_init(consumer_lock);
 	pthread_mutex_init(monitor_lock);
-	int consumer_location = 0;
-	int producer_location = 0;
-	int buffer_data_stored = 0;
+	int consumer_location;
+	int producer_location;
+	int buffer_data_stored;
+	int *buffer;
 } monitor;
